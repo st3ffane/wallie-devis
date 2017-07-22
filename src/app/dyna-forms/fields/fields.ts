d30551ec24a7168a63c89c4353176d4379ee4955
st3ffane <steph.ponteins@gmail.com>
2017-07-22 15:45:45 +0200

    mise en place tabs

--- src/app/dyna-forms/fields/fields.ts
+++ src/app/dyna-forms/fields/fields.ts
@@ -1,10 +1,12 @@
 import {DynaArborescenceComponent} from "./dyna.arborescence.component";
 import {DynaArborescence2Component} from "./dyna.arborescence2.component";
+import {TabComponent} from "./tab.forms";
 import {EXPEDOM} from "./expedom/expedom";
 
 
 export const FIELDS = [
   DynaArborescenceComponent,
   DynaArborescence2Component,
+  TabComponent,
   ...EXPEDOM
 ];
--- src/app/dyna-forms/fields/tab.forms.ts
+++ src/app/dyna-forms/fields/tab.forms.ts
@@ -1,21 +1,45 @@
 import {Component, Input, ChangeDetectorRef} from "@angular/core";
-import {FormGroup} from "@angular/forms";
+import {FormGroup, FormControl} from "@angular/forms";
 import {DevisProvider} from "../../providers/devis.provider";
 
 @Component({
-  selector:"exp-expert",
-  templateUrl:"./expert.taux.html",
-  styleUrls:["./expert.taux.scss"]
+  selector:"tab-forms",
+  templateUrl:"./tab.forms.html",
+  styleUrls:["./tab.forms.scss"]
 })
-export class ExpertTaux {
+export class TabComponent {
     @Input()question:any;
     @Input() form:FormGroup;
     //@Input() formulaire;//pour pouvoir faire les modifications
+    //choix du filtre
+    _filter:string ;
+    filter_form_ctrl = null;//nom du control text (angular)
+
+    get filter(){return this._filter;}
 
      constructor( private _devis:DevisProvider,
         private _changeRef:ChangeDetectorRef){}
 
     ngOnInit(){
+      this.create_forms_elements();
+      //recup le 1er id comme valeur de l'input
+      if(this.question){
+        this._filter = this.question.options[0].id;
+      }
+    }
 
+    /**
+     * Creation des elements du formulaire pour ce control
+     * angular specific
+     */
+    private create_forms_elements(){
+      this.filter_form_ctrl = "tabs_"+this.question.id;
+      this.form.addControl(this.filter_form_ctrl, new FormControl(''));
+        //zone de recherche        
+        /*this.filter_form_ctrl = "gps_search_"+this.question.id
+        
+
+        this.filter_form_ctrl = "gps_filter_"+this.question.id;
+        this.form.addControl(this.filter_form_ctrl, new FormControl(''));*/
     }
 }
